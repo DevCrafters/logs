1f29e9127118d61f1b8a267f90c2e2241215a6f4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-23 11:03:53 +0000

    Deploy DevCrafters/logs to github.com/DevCrafters/logs.git:gh-pages

--- README.md
+++
@@ -1,8 +0,0 @@
-# logs
-
-Contains the log of discord server
-
-## License
-
-MIT for codes (`src/**/*`)
-CC BY-NC-SA for documents (`docs/**/*`)
--- src/custom-typings/markdown-it-attrs/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'markdown-it-attrs' {
-  import MarkdownIt from 'markdown-it';
-
-  const Plugin: (md: MarkdownIt) => void;
-
-  export default Plugin;
-}
